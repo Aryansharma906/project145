bcd14015b98f29f737df9eae59d3e4ee5c88d3dd
Aryansharma906 <76518533+Aryansharma906@users.noreply.github.com>
2023-07-01 10:49:34 +0530

    Add files via upload

---
+++ vr.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+    </head>
+    <body>
+        <a-scene background="color: #000000">
+            <a-box position="-1,0.5,-3" color="#4CC3D9"></a-box>
+            <a-sphere position="-0,2.5,-5" color="#4CC3D9"></a-box>
+            <a-octahaderon position="-2.3,0.43,0" color ="#FF926B"></a-box>
+                
+
+        </a-scene>
+    </body>
+</html>
